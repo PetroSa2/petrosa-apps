69cf0d2e4747f4b2bcb81b94b4be635c8c215f42
@yurisa2 <ae_ysa@contractor.indeed.com>
2024-07-04 17:00:44 -0300

    adding external producer

--- petrosa/messaging/pubsubproducer.py
+++ petrosa/messaging/pubsubproducer.py
@@ -1,9 +1,14 @@
 from google.cloud import pubsub_v1
 
 
-def send_msg(data: str, project_id: str, topic_id: str):
-    publisher = pubsub_v1.PublisherClient()
-    topic_path = publisher.topic_path(project_id, topic_id)
+def get_producer(project_id: str, topic_id: str):
+    producer = pubsub_v1.PublisherClient()
+    topic_path = producer.topic_path(project_id, topic_id)
+
+    return producer, topic_path
+
+
+def send_msg(data: str, publisher, topic_path: str):
 
     publish_future = publisher.publish(topic_path, data.encode("utf-8"))
         
